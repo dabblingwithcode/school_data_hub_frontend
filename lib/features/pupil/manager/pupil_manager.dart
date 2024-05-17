e7e627d2460df7d23e6230cc28203658cecc179b
Nacho <nacho.dominguis@proton.me>
2024-05-18 01:52:50 +0200

    remove client, connect endpoints, clean up and add comments

--- lib/features/pupil/manager/pupil_manager.dart
+++ lib/features/pupil/manager/pupil_manager.dart
@@ -1,16 +1,13 @@
 import 'dart:async';
-import 'dart:convert';
 import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:schuldaten_hub/api/api.dart';
-import 'package:schuldaten_hub/api/services/api_manager.dart';
 import 'package:schuldaten_hub/common/constants/enums.dart';
 import 'package:schuldaten_hub/common/services/locator.dart';
 import 'package:schuldaten_hub/common/services/notification_manager.dart';
-import 'package:schuldaten_hub/common/services/session_manager.dart';
 import 'package:schuldaten_hub/common/utils/custom_encrypter.dart';
 import 'package:schuldaten_hub/common/utils/debug_printer.dart';
 import 'package:schuldaten_hub/features/attendance/models/missed_class.dart';
@@ -25,8 +22,6 @@ class PupilManager extends ChangeNotifier {
   final _pupils = <int, PupilProxy>{};
 
   List<PupilProxy> get allPupils => _pupils.values.toList();
-
-  final client = locator.get<ApiManager>().dioClient.value;
 
   PupilManager();
 
@@ -43,7 +38,6 @@ class PupilManager extends ChangeNotifier {
     }
     debug.warning('availablePupils im PupilManager $pupilsToFetch');
     await fetchPupilsByInternalId(pupilsToFetch);
-    notifyListeners();
   }
 
   Future<void> updatePupilList(List<PupilProxy> pupils) async {
@@ -53,31 +47,28 @@ class PupilManager extends ChangeNotifier {
   //- Fetch pupils with the given ids from the backend
   Future fetchPupilsByInternalId(List<int> internalPupilIds) async {
     locator<NotificationManager>().isRunningValue(true);
-    // we request the data posting a json with the id list - let's build that
-    final pupilIdList = jsonEncode({"pupils": internalPupilIds});
-    // and a list to manipulate the matched pupils
-    List<int> outdatedPupilPersonalDataIds = [];
-    // request
-    final response =
-        await client.post(EndpointsPupil.fetchPupilsUrl, data: pupilIdList);
-    debug.info('PupilProxy request sent!');
-    // we have the response - let's build unidentified Pupils with it
-    final fetchedPupils =
-        (response.data as List).map((e) => Pupil.fromJson(e)).toList();
 
-    outdatedPupilPersonalDataIds = internalPupilIds
+    // fetch the pupils from the backend
+    final fetchedPupils = await EndpointsPupil()
+        .fetchListOfPupils(internalPupilIds: internalPupilIds);
+
+    // check if we did not get a pupil response for some ids
+    // if so, we will delete the personal data for those ids later
+    final List<int> outdatedPupilPersonalDataIds = internalPupilIds
         .where((element) =>
             !fetchedPupils.any((pupil) => pupil.internalId == element))
         .toList();
 
-    // now we match them with their personal data
+    // now we match the pupils from the response with their personal data
     final personalDataManager = locator.get<PupilPersonalDataManager>();
     for (Pupil fetchedPupil in fetchedPupils) {
       final proxyInRepository = _pupils[fetchedPupil.internalId];
-
       if (proxyInRepository != null) {
         proxyInRepository.updatePupil(fetchedPupil);
       } else {
+        // if the pupil is not in the repository, that would be weird
+        // since we did not send the id to the backend
+
         final personalData =
             personalDataManager.getPersonalData(fetchedPupil.internalId);
 
@@ -85,7 +76,10 @@ class PupilManager extends ChangeNotifier {
             PupilProxy(pupil: fetchedPupil, personalData: personalData);
       }
     }
-    // remove the outdated pupilbase elements
+
+    // remove the outdated pupilbase elements that
+    // did not get a response from the backend
+
     if (outdatedPupilPersonalDataIds.isNotEmpty) {
       final deletedPupils = await personalDataManager
           .deletePupilBaseElements(outdatedPupilPersonalDataIds);
@@ -102,6 +96,13 @@ class PupilManager extends ChangeNotifier {
     _pupils.clear();
   }
 
+  void updatePupilProxyWithPupil(Pupil pupil) {
+    final proxy = _pupils[pupil.internalId];
+    if (proxy != null) {
+      proxy.updatePupil(pupil);
+    }
+  }
+
   void updatePupilsFromMissedClasses(List<MissedClass> allMissedClasses) {
     for (MissedClass missedClass in allMissedClasses) {
       final missedPupil = _pupils[missedClass.missedPupilId];
@@ -113,101 +114,90 @@ class PupilManager extends ChangeNotifier {
 
       missedPupil.updateFromAllMissedClasses(allMissedClasses);
     }
-  }
-
-  void updatePupilFromResponse(Map<String, dynamic> pupilResponse) {
-    // the response comes as a json - let's make a pupil
-    final Pupil responsePupil = Pupil.fromJson(pupilResponse);
-    _pupils[responsePupil.internalId]!.updatePupil(responsePupil);
+    notifyListeners();
   }
 
   Future<void> postAvatarImage(
     File imageFile,
-    PupilProxy pupil,
+    PupilProxy pupilProxy,
   ) async {
+    // first we encrypt the file
+
     final encryptedFile = await customEncrypter.encryptFile(imageFile);
-    // send request
+
+    // Now we prepare the form data for the request.
+
     String fileName = encryptedFile.path.split('/').last;
-    // Prepare the form data for the request.
     var formData = FormData.fromMap({
       'file': await MultipartFile.fromFile(
         encryptedFile.path,
         filename: fileName,
       ),
     });
-    // send request
-    final Response response = await client.patch(
-      EndpointsPupil().updatePupilhWithAvatarUrl(pupil.internalId),
-      data: formData,
+
+    // send the Api request
+
+    final Pupil pupilUpdate = await EndpointsPupil().updatePupilWithAvatar(
+      id: pupilProxy.internalId,
+      formData: formData,
     );
-    // Handle errors.
-    if (response.statusCode != 200) {
-      debug.warning('Something went wrong with the multipart request');
-    }
 
-    // Success! We have a pupil response - let's patch the pupil with the data
+    // update the pupil in the repository
 
-    final Map<String, dynamic> pupilResponse = response.data;
+    pupilProxy.updatePupil(pupilUpdate);
+
+    // Delete the outdated encrypted file.
+
     final cacheManager = DefaultCacheManager();
-    final cacheKey = pupil.internalId;
+    final cacheKey = pupilProxy.internalId;
 
     cacheManager.removeFile(cacheKey.toString());
-
-    updatePupilFromResponse(pupilResponse);
   }
 
   Future<void> deleteAvatarImage(int pupilId, String cacheKey) async {
-    // send request
-    final Response response = await client.delete(
-      EndpointsPupil().deletePupilAvatarUrl(pupilId),
-      options: Options(headers: {
-        'x-access-token': locator<SessionManager>().credentials.value.jwt
-      }),
+    // send the Api request
+    await EndpointsPupil().deletePupilAvatar(
+      internalId: pupilId,
     );
-    // Handle errors.
-    if (response.statusCode != 200) {
-      debug.warning('Something went wrong deleting the avatar');
-      return;
-    }
-    // Delete the cached image
+
+    // Delete the outdated encrypted file in the cache.
+
     final cacheManager = DefaultCacheManager();
     await cacheManager.removeFile(cacheKey);
+
     // and update the repository
-    _pupils[pupilId]!.clearAvtar();
+    _pupils[pupilId]!.clearAvatar();
   }
 
   Future<void> patchPupil(int pupilId, String jsonKey, var value) async {
     locator<NotificationManager>().isRunningValue(true);
-    //- if the value is relevant to siblings, check for siblings first and handle it if true
+
+    // if the value is relevant to siblings, check for siblings first and handle them if true
 
     if (jsonKey == 'communication_tutor1' ||
         jsonKey == 'communication_tutor2' ||
         jsonKey == 'parents_contact') {
       final PupilProxy pupil = findPupilById(pupilId);
       if (pupil.family != null) {
+        // we have a sibling
         // create list with ids of all pupils with the same family value
+
         final List<int> pupilIdsWithSameFamily = _pupils.values
             .where((p) => p.family == pupil.family)
             .map((p) => p.internalId)
             .toList();
 
-        final siblingsPatchData = jsonEncode({
-          jsonKey: value,
-          'pupils': pupilIdsWithSameFamily,
-        });
-        final Response siblingsResponse = await client
-            .patch(EndpointsPupil.patchSiblingsUrl, data: siblingsPatchData);
-        if (siblingsResponse.statusCode != 200) {
-          locator<NotificationManager>().showSnackBar(
-              NotificationType.warning, 'Fehler beim Patchen der Geschwister!');
-          locator<NotificationManager>().isRunningValue(false);
-          return;
-        }
-        // let's update the siblings
-        final List<Pupil> siblingPupils = (siblingsResponse.data as List)
-            .map((e) => Pupil.fromJson(e))
-            .toList();
-        for (Pupil sibling in siblingPupils) {
+        // call the endpoint to update the siblings
+
+        final List<Pupil> siblingsUpdate = await EndpointsPupil()
+            .updateSiblingsProperty(
+                siblingsPupilIds: pupilIdsWithSameFamily,
+                property: jsonKey,
+                value: value);
+
+        // now update the siblings with the new data
+
+        for (Pupil sibling in siblingsUpdate) {
           _pupils[sibling.internalId]!.updatePupil(sibling);
         }
 
@@ -216,23 +206,16 @@ class PupilManager extends ChangeNotifier {
         locator<NotificationManager>().isRunningValue(false);
       }
     }
-    // prepare the data for the request
-    final data = jsonEncode({jsonKey: value});
-    final Response response = await client
-        .patch(EndpointsPupil().updatePupilPropertyUrl(pupilId), data: data);
-    // we have a response
-    final Map<String, dynamic> pupilResponse = response.data;
-    // handle errors
-    if (response.statusCode != 200) {
-      locator<NotificationManager>().showSnackBar(
-          NotificationType.warning, 'Fehler beim Patchen des Schülers!');
-      locator<NotificationManager>().isRunningValue(false);
-      return;
-    }
-    // let's patch the pupil with the response
-    updatePupilFromResponse(pupilResponse);
-    locator<NotificationManager>().showSnackBar(
-        NotificationType.success, 'Schüler erfolgreich gepatcht!');
+
+    // The pupil is no sibling. Make the api call for the single pupil
+
+    final Pupil pupilUpdate = await EndpointsPupil()
+        .updatePupilProperty(id: pupilId, property: jsonKey, value: value);
+
+    // now update the pupil in the repository
+
+    _pupils[pupilId]!.updatePupil(pupilUpdate);
+
     locator<NotificationManager>().isRunningValue(false);
   }
 
